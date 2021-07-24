f9ea5c966bf205e6a9679826f6523fb68e32b191
allitail <a.t.alli99u@gmail.com>
2021-07-24 22:56:38 +0900

    OMDbAPI.COM movie

---
+++ src/components/Loader.vue
@@ -0,0 +1,26 @@
+<template>
+    <div :class="{ absolute: absolute }" class="spinner-border text-primary"></div>
+</template>
+
+<script>
+export default {
+    props: {
+        size: {
+            type: Number,
+            default: 2
+        },
+        absolute: {
+            type: Boolean,
+            default: false
+        },
+        fixed: {
+            type: Boolean,
+            default: false
+        },
+        zIndex: {
+            type: Number,
+            default: 0
+        }
+    }
+}
+</script>
